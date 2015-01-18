03954c865f8bd9fe4c564d9659447d4f039d51e5
neojato <kpaul86@gmail.com>
2015-01-18 15:34:00 -0600

    Step 10: The transclude property

--- scripts/directives/uiDirectives.js
+++ scripts/directives/uiDirectives.js
@@ -3,9 +3,10 @@ angular.module('ui.directives', [])
     'use strict';
     return {
       restrict: 'AEC',
+      transclude: true,
       scope: {
         content: '=myContent'
       },
-      template: '<div class="background" data-ng-show="content.visible"></div><div class="lightbox" data-ng-show="content.visible"><button data-ng-click="content.toggleView()">close</button><p>{{content.text}}</p></div>'
+      template: '<div class="background" data-ng-show="content.visible"></div><div class="lightbox" data-ng-show="content.visible"><button data-ng-click="content.toggleView()">close</button><div data-ng-transclude></div></div>'
     };
   });
