9b012bd01dea0581a80a89f161d21c943d1ae706
neojato <kpaul86@gmail.com>
2015-01-18 13:27:00 -0600

    Step 3: Add a controller

--- scripts/modules/ui.js
+++ scripts/modules/ui.js
@@ -1,1 +1,6 @@
-angular.module('ui.module', []);
+angular.module('ui.module', [])
+  .controller('uiCtrl', ['$scope', function($scope) {
+    'user strict';
+    $scope.title = 'DOM manipulation with AngularJS';
+    $scope.lightboxText = 'Some text that should be displayed in a lightbox';
+  }]);
